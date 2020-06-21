99d08d679ab1dd0a94fe204268e8ebeb858374a0
jeanchristophehenry <56442989+jeanchristophehenry@users.noreply.github.com>
2020-06-21 05:42:45 +0200

    Correction log

--- iptable-configuration.sh
+++ iptable-configuration.sh
@@ -101,10 +101,10 @@ iptables -t mangle -A PREROUTING -s 169.254.0.0/16 -j DROP
 iptables -t mangle -A PREROUTING -s 172.16.0.0/12 -j DROP
 iptables -t mangle -A PREROUTING -s 192.0.2.0/24 -j DROP
 iptables -t mangle -A PREROUTING -s 192.168.0.0/16 -j DROP
-iptables -t mangle -A PREROUTING -s 10.0.0.0/8 -j DROP --log-prefix "** Block spoofed **"
-iptables -t mangle -A PREROUTING -s 0.0.0.0/8 -j DROP --log-prefix "** Block spoofed **"
-iptables -t mangle -A PREROUTING -s 240.0.0.0/5 -j DROP --log-prefix "** Block spoofed **"
-iptables -t mangle -A PREROUTING -s 127.0.0.0/8 ! -i lo -j DROP  --log-prefix "** Block spoofed **"
+iptables -t mangle -A PREROUTING -s 10.0.0.0/8 -j DROP
+iptables -t mangle -A PREROUTING -s 0.0.0.0/8 -j DROP
+iptables -t mangle -A PREROUTING -s 240.0.0.0/5 -j DROP
+iptables -t mangle -A PREROUTING -s 127.0.0.0/8 ! -i lo -j DROP
 
 ### 6: Drop ICMP (you usually don't need this protocol) ### 
 iptables -t mangle -A PREROUTING -p icmp -j DROP  
