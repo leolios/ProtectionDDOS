1869dac6a806092a828ace6076927182b3c0c5f9
jeanchristophehenry <56442989+jeanchristophehenry@users.noreply.github.com>
2020-06-21 05:19:07 +0200

    a

--- iptable-configuration.sh
+++ iptable-configuration.sh
@@ -5,22 +5,22 @@
 #     Ajout des filtres           #
 ###################################
 
-/sbin/iptables -P INPUT DROP
-/sbin/iptables -t filter -N LOG_N_ACCEPT
-/sbin/iptables -t filter -A LOG_N_ACCEPT -j LOG --log-level warning --log-prefix "ACTION=INPUT-ACCEPT "
-/sbin/iptables -t filter -A LOG_N_ACCEPT -j ACCEPT
+iptables -P INPUT DROP
+iptables -t filter -N LOG_N_ACCEPT
+iptables -t filter -A LOG_N_ACCEPT -j LOG --log-level warning --log-prefix "ACTION=INPUT-ACCEPT "
+iptables -t filter -A LOG_N_ACCEPT -j ACCEPT
 
 ###################################
 #     première configuration      #
 ###################################
 
-/sbin/iptables -A INPUT -i eno1 -j LOG_N_ACCEPT                                      # Autoriser les flux en localhost
-/sbin/iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j LOG_N_ACCEPT  # Autoriser les connexions déjà établies,
-/sbin/iptables -A INPUT -p tcp -m tcp --dport 22 -j LOG_N_ACCEPT                    # Autoriser SSH,
-/sbin/iptables -A INPUT -p tcp -m tcp --dport http -j LOG_N_ACCEPT                  # Autoriser HTTP,
-/sbin/iptables -A INPUT -p tcp -m tcp --dport https -j LOG_N_ACCEPT                 # Autoriser HTTPS,
-/sbin/iptables -P INPUT DROP                                                 # Politique par défaut de la table INPUT : DROP. (i.e bloquer tout le reste).
-/sbin/iptables -P FORWARD DROP                                               # On est pas un routeur ou un NAT pour un réseau privé, on ne forward pas de paquet.
+iptables -A INPUT -i eno1 -j LOG_N_ACCEPT                                      # Autoriser les flux en localhost
+iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j LOG_N_ACCEPT  # Autoriser les connexions déjà établies,
+iptables -A INPUT -p tcp -m tcp --dport 22 -j LOG_N_ACCEPT                    # Autoriser SSH,
+iptables -A INPUT -p tcp -m tcp --dport http -j LOG_N_ACCEPT                  # Autoriser HTTP,
+iptables -A INPUT -p tcp -m tcp --dport https -j LOG_N_ACCEPT                 # Autoriser HTTPS,
+iptables -P INPUT DROP                                                 # Politique par défaut de la table INPUT : DROP. (i.e bloquer tout le reste).
+iptables -P FORWARD DROP                                               # On est pas un routeur ou un NAT pour un réseau privé, on ne forward pas de paquet.
 
 
 ###################################
@@ -49,26 +49,26 @@
 /sbin/iptables -t mangle -A PREROUTING -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -j DROP  
 
 ### 4: Block packets with bogus TCP flags ### 
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG NONE -j DROP --log-prefix "** Block TCP **" 
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP --log-prefix "** Block TCP **" 
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags SYN,RST SYN,RST -j DROP --log-prefix "** Block TCP **"
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,RST FIN,RST -j DROP --log-prefix "** Block TCP **"
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,ACK FIN -j DROP --log-prefix "** Block TCP **"
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,URG URG -j DROP --log-prefix "** Block TCP **"
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,FIN FIN -j DROP --log-prefix "** Block TCP **"
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,PSH PSH -j DROP --log-prefix "** Block TCP **"
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL ALL -j DROP --log-prefix "** Block TCP **"
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL NONE -j DROP --log-prefix "** Block TCP **"
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP --log-prefix "** Block TCP **"
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,FIN,PSH,URG -j DROP --log-prefix "** Block TCP **"
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP  --log-prefix "** Block TCP **"
+/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG NONE -j DROP 
+/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP 
+/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags SYN,RST SYN,RST -j DROP 
+/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
+/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,ACK FIN -j DROP
+/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,URG URG -j DROP
+/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,FIN FIN -j DROP
+/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,PSH PSH -j DROP
+/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL ALL -j DROP
+/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL NONE -j DROP
+/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP
+/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,FIN,PSH,URG -j DROP
+/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
 
 ### 5: Block spoofed packets ### 
-/sbin/iptables -t mangle -A PREROUTING -s 224.0.0.0/3 -j DROP --log-prefix "** Block spoofed **"
-/sbin/iptables -t mangle -A PREROUTING -s 169.254.0.0/16 -j DROP --log-prefix "** Block spoofed **"
-/sbin/iptables -t mangle -A PREROUTING -s 172.16.0.0/12 -j DROP --log-prefix "** Block spoofed **"
-/sbin/iptables -t mangle -A PREROUTING -s 192.0.2.0/24 -j DROP --log-prefix "** Block spoofed **"
-/sbin/iptables -t mangle -A PREROUTING -s 192.168.0.0/16 -j DROP --log-prefix "** Block spoofed **"
+/sbin/iptables -t mangle -A PREROUTING -s 224.0.0.0/3 -j DROP
+/sbin/iptables -t mangle -A PREROUTING -s 169.254.0.0/16 -j DROP
+/sbin/iptables -t mangle -A PREROUTING -s 172.16.0.0/12 -j DROP
+/sbin/iptables -t mangle -A PREROUTING -s 192.0.2.0/24 -j DROP
+/sbin/iptables -t mangle -A PREROUTING -s 192.168.0.0/16 -j DROP
 /sbin/iptables -t mangle -A PREROUTING -s 10.0.0.0/8 -j DROP --log-prefix "** Block spoofed **"
 /sbin/iptables -t mangle -A PREROUTING -s 0.0.0.0/8 -j DROP --log-prefix "** Block spoofed **"
 /sbin/iptables -t mangle -A PREROUTING -s 240.0.0.0/5 -j DROP --log-prefix "** Block spoofed **"
@@ -128,7 +128,8 @@ iptables -I OUTPUT -m state -p udp -s 127.0.0.1 ! -d 127.0.0.1 -j LOG --log-pref
 #         Fin du script           #
 ###################################
 
-tail -f /var/log/kern.log
 apt-get install iptables-persistent
 iptables-save > /etc/iptables/rules.v4
 systemctl restart rsyslog
+
+tail -f /var/log/kern.log
